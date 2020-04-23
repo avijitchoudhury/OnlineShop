22b75987c0e058254486f5c5169f78e28e426ab9
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-04-23 14:21:38 -0400

    adding more routes to the navigation

--- routes/shop.js
+++ routes/shop.js
@@ -8,4 +8,10 @@ const productsController = require('../controllers/products')
 
 router.get('/', productsController.getProducts);
 
+router.get('/products');
+
+router.get('/cart');
+
+router.get('checkout');
+
 module.exports = router;
