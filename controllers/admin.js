cb664bc0631552d70dbb2f4a89d3c78e1e9fadb4
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-05-28 13:21:30 -0400

    registering new routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product')
 
+
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
@@ -17,16 +18,11 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll(products => {
-    res.render('shop/product-list', {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
       prods: products,
-      docTitle: 'Avijit\'s Shop',
-      path: '/',
-      pageTitle: 'Shop',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
 };
-
