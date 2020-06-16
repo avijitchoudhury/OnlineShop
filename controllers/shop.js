b77c49cbfe332c93f67c81de3761e3954ca3dc06
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-06-16 11:24:56 -0400

    added mobile design and view and made cart more dynamic

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,12 +1,22 @@
-const Product = require('../models/product')
-
+const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-   Product.fetchAll(products => {
+  Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      path: '/products',
       pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
     });
   });
 };
@@ -15,30 +25,35 @@ exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
       prods: products,
-      path: '/',
-      pageTitle: 'Shop'
+      pageTitle: 'Shop',
+      path: '/'
     });
   });
-}
+};
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
-  })
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
 }
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
-    pageTitle: 'Your orders'
-  })
-}
-
+    pageTitle: 'Your Orders'
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout'
-  })
-}
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(process.mainModule.filename),
+const p = path.join(
+  path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
     } else {
       cb(JSON.parse(fileContent));
     }
-  })
-}
+  });
+};
 
 module.exports = class Product {
   constructor(title, imageUrl, description, price) {
@@ -25,15 +26,23 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
-      })
+      });
     });
   }
 
   static fetchAll(cb) {
-   getProductsFromFile(cb);
-}
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
 };
