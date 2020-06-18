77bdb3f20ebea336616887fe42c198b7c5fd338c
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-06-18 16:12:47 -0400

    editing product data, implementing deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -25,7 +25,7 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    if(!product) {
+    if (!product) {
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
@@ -34,12 +34,25 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product
     });
-  })
+  });
 };
 
 exports.postEditProduct = (req, res, next) => {
-  
-}
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -50,3 +63,7 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+}
