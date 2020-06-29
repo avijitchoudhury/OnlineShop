da56c3e59ba77251c1f49b45c46a1b9e35fac0c0
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-06-29 16:40:13 -0400

    deleting cart items and fixed a delete product bug

--- models/cart.js
+++ models/cart.js
@@ -38,13 +38,16 @@ module.exports = class Cart {
     });
   }
 
-  static deleteProduct(price, productPrice) {
+  static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
       if(err) {
         return
       }
       const updatedCart = {...JSON.parse(fileContent)};
       const product = updatedCart.products.find(prod => prod.id === id);
+      if(!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         prod => prod.id !== id
@@ -52,7 +55,7 @@ module.exports = class Cart {
       updatedCart.totalPrice = 
       updatedCart.totalPrice - productPrice * productQty;
 
-      fs.writeFile(p, JSON.stringify(cart), err => {
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
       });
     })
