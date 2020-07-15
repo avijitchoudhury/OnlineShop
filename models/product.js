472363dee1c3080978d507016ac570e84a6607a4
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-07-15 10:46:26 -0400

    inserting data and creating product

--- models/product.js
+++ models/product.js
@@ -10,7 +10,7 @@ const Product = sequelize.define('product', {
     primaryKey: true
   },
   title: Sequelize.STRING,
-  priice: {
+  price: {
     type: Sequelize.DOUBLE,
     allowNull: false
   },
