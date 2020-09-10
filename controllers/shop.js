86d23a02fec076554435ab66cfd222050a57215e
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-09-10 12:55:18 -0400

    deleting products through admin panel

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,44 +11,44 @@ exports.getProducts = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
     });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({where: { id: prodId } })
-  // .then(products => {
-  //   res.render('shop/product-detail', {
-  //     product: product[0],
-  //     pageTitle: product[0].title,
-  //     path: '/products'
-  //   });
-  // })
-  // .catch(err => console.log(err))
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
   Product.findById(prodId)
-  .then(product => {
+    .then(product => {
       res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
         path: '/products'
       });
-  })
-  .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
   Product.findAll()
-  .then(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => {
-    console.log(err)
-  })
 };
 
 exports.getCart = (req, res, next) => {
@@ -56,9 +56,11 @@ exports.getCart = (req, res, next) => {
     Product.fetchAll(products => {
       const cartProducts = [];
       for (product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id)
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
         if (cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty});
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
       res.render('shop/cart', {
@@ -66,8 +68,8 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart',
         products: cartProducts
       });
-    })
-  })
+    });
+  });
 };
 
 exports.postCart = (req, res, next) => {
