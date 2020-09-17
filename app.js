e2ce4faf7dc3603a5e614eac934cbdb9d3299ed7
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-09-17 13:48:27 -0400

    creating and fetching a cart

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
 // .sync({ force: true })
-.sync({ force: true })
+.sync()
 .then(result => {
   return User.findByPk(1);
 })
@@ -64,7 +64,10 @@ sequelize
   return user;
 })
 .then(user => {
-  console.log(user);
+  // console.log(user);
+  return user.createCart();
+})
+.then(cart => {
   app.listen(3000);
 })
 .catch(err => {
