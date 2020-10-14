d14d070b71859621148bfe89408b98f3ac312d50
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-10-14 11:07:27 -0400

    refactoring code to implement MongoDB database

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql', 
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://avi:avijit123@cluster0.errje.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+module.exports = mongoConnect;
