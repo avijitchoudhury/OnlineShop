658864e5fb95edbf26006ffe3b923b957e0668e9
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-10-28 11:44:00 -0400

    Creating functionality for add to cart

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5f985076dc1fc6c9df33aa32')
   .then(user => {
-    req.user = user;
+    req.user = new User (user.name, user.email, user.cart, user._id );
     next();
   })
   .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectID;
 
 class User {
-  constructor(username, email, cart) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
     this.cart = cart;
@@ -17,11 +17,12 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex(cp => {
-    //   return cp._id === product._id;
-    // })
-
-    const updatedCart = {items: [{ ...product, quantity: 1 }] }
+    const cartProduct = this.cart.items.findIndex(cp => {
+      return cp.productId === product._id;
+    });
+    const updatedCart = {
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }] 
+    };
     const db = getDb();
     return db
     .collection('users')
